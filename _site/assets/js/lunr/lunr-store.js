a35f13455490f1b40e1cb2d6887668e27d237621
Legund <unknownlegund@hotmail.com>
2019-12-03 10:46:33 -0700

    unet results post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "I was born",
+        "excerpt":"Born today. Was pretty good.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/1993/03/15/birth.html",
+        "teaser":null}]
